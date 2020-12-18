1afe35a06b060b7651ad8a4872eb76f2226312b8
Aditya Cherukuri <adityacherukuri@adityas-mbp.lan>
2020-12-18 15:07:29 -0500

    deploy v1

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 // set up express
@@ -8,15 +9,6 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-
-const PORT = process.env.PORT || 5000;
-
-const budget = require("./budget.json");
-app.get("/budget", (req, res) => {
-  res.json(budget);
-});
-
-app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
 
 // set up mongoose
 
@@ -33,8 +25,26 @@ mongoose.connect(
   }
 );
 
+const budget = require("./budget.json");
+app.get("/budget", (req, res) => {
+  res.json(budget);
+});
+
 // set up routes
 
 app.use("/users", require("./routes/userRouter"));
 app.use("/todos", require("./routes/todoRouter"));
 app.use("/budgets", require("./routes/budgetRouter"));
+
+//serving static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
